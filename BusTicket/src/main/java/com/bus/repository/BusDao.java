4306c3c49d72d81c4e0414aca78d8edda7054674
Akhiltop <akhilyadlapalli@gmail.com>
2023-02-22 23:26:17 +0530

    added day2 akhil

--- BusTicket/src/main/java/com/bus/repository/BusDao.java
+++ BusTicket/src/main/java/com/bus/repository/BusDao.java
@@ -1,11 +1,19 @@
 package com.bus.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bus.model.Bus;
 
+
+
 public interface BusDao extends JpaRepository<Bus, Integer>{
 	
-	
+	@Query("select * from bus b where b.routefrom=:from and b.routeto=:to")
+	public List<Bus> getAllBusByRoute(@Param("from") String from,@Param("to") String to);
 
+	
 }

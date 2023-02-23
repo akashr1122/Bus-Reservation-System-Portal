ef1d66ce7857c1b7e7bf4c398337681b6eb4b01b
akashr1122 <akashsingh4321a@gmail.com>
2023-02-24 00:05:48 +0530

    add day-3 work

--- BusTicket/src/main/java/com/bus/service/IFeedBackService.java
+++ BusTicket/src/main/java/com/bus/service/IFeedBackService.java
@@ -2,16 +2,17 @@ package com.bus.service;
 
 import java.util.List;
 
+import com.bus.exception.FeedBackNotFoundException;
 import com.bus.model.FeedBack;
 
 public interface IFeedBackService {
 
 	
-	   FeedBack addFeedBack(FeedBack feedback);
+	   FeedBack addFeedBack(FeedBack feedback) ;
   
-	   FeedBack updateFeedBack (FeedBack feedback);
+	   FeedBack updateFeedBack (FeedBack feedback) throws FeedBackNotFoundException;
 
-	   FeedBack viewFeedBack(int feedbackld);
+	   FeedBack viewFeedBack(int feedbackld) throws FeedBackNotFoundException;
  
-	   List<FeedBack> viewAllFeedBack();
+	   List<FeedBack> viewAllFeedBack() throws FeedBackNotFoundException;
 }
---
+++ BusTicket/src/main/java/com/bus/service/IUserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.bus.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bus.exception.UserNotFoundException;
+import com.bus.model.User;
+import com.bus.repository.UserDao;
+
+public class IUserServiceImpl implements IUserService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Override
+	public User addUser(User user) {
+
+		User u = userDao.save(user);
+		return u;
+
+	}
+
+	@Override
+	public User updateUser(User user) throws UserNotFoundException {
+
+		userDao.findById(user.getUserId()).orElseThrow(() -> new UserNotFoundException("This User is not present .."));
+		User u = userDao.save(user);
+
+		return u;
+
+	}
+
+	@Override
+	public String deleteUser(int userld) throws UserNotFoundException {
+
+		User u = userDao.findById(userld)
+				.orElseThrow(() -> new UserNotFoundException(" Useris not present .. which id is " + userld));
+
+		userDao.delete(u);
+		return "User deleted Sucessfully";
+
+	}
+
+	@Override
+	public User viewUser(int userld) throws UserNotFoundException {
+
+		User u = userDao.findById(userld)
+				.orElseThrow(() -> new UserNotFoundException(" Useris not present .. which id is " + userld));
+
+		return u;
+	}
+
+	@Override
+	public List<User> viewAllUsers() throws UserNotFoundException {
+
+		List<User> users = userDao.findAll();
+		if (users.isEmpty())
+			throw new UserNotFoundException("No any User  present");
+		return users;
+	}
+
+}

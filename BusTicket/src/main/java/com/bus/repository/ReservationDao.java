85cc2011e40f1c033381f53ea8395bfe69177d99
Akhiltop <akhilyadlapalli@gmail.com>
2023-02-23 23:05:55 +0530

    added Day3 akhil

--- BusTicket/src/main/java/com/bus/repository/ReservationDao.java
+++ BusTicket/src/main/java/com/bus/repository/ReservationDao.java
@@ -1,9 +1,17 @@
 package com.bus.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bus.model.Reservation;
 
 public interface ReservationDao extends JpaRepository<Reservation, Integer>{
 
+	
+	public List<Reservation> findByReservationDate(LocalDate localDate);
+	
+	
+	
 }

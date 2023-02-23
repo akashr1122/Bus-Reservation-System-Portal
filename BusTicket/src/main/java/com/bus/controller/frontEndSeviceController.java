ebb37b6aab923590c7a7745aef81759ddceaba38
awresh <experimentsworlds@gmail.com>
2023-02-23 16:01:42 +0530

    day-3

--- BusTicket/src/main/java/com/bus/controller/frontEndSeviceController.java
+++ BusTicket/src/main/java/com/bus/controller/frontEndSeviceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.bus.frontEnd.frontEndService;
 import com.bus.model.Bus;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class frontEndSeviceController {
 
 	@Autowired

a0b24d2232705c4cfa154552a2971444d2883c1a
Akhiltop <akhilyadlapalli@gmail.com>
2023-02-24 00:52:01 +0530

    added Day4 akhil

--- BusTicket/src/main/java/com/bus/service/IUserServiceImpl.java
+++ BusTicket/src/main/java/com/bus/service/IUserServiceImpl.java
@@ -3,11 +3,14 @@ package com.bus.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bus.exception.UserNotFoundException;
 import com.bus.model.User;
 import com.bus.repository.UserDao;
 
+
+@Service
 public class IUserServiceImpl implements IUserService {
 
 	@Autowired

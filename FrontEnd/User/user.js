a9408dcda6b8085f0aab2f2f26da753a7429c6c4
Awresh <experiments.worlds@gmail.com>
2023-02-25 00:23:16 +0530

    day4

--- FrontEnd/User/user.js
+++ FrontEnd/User/user.js
@@ -14,6 +14,30 @@ async function weekdata(url) {
     }
 }
 
+
+//  POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+
+
+
+
 // for Create From and to option
 (async () => {
     let from = await weekdata('http://localhost:8080/bus/routeFrom')
@@ -54,6 +78,67 @@ async function weekdata(url) {
 async function AvailableBusInRoot() {
     let from = document.getElementById("From").value;
     let to = document.getElementById("To").value;
-    let bus= await weekdata(`http://localhost:8080/bus/route/${from}/${to}`)
-    console.log(bus);
+    let bus = await weekdata(`http://localhost:8080/bus/route/${from}/${to}`);
+    function createElement(ele) {
+        let ele2 = document.createElement(ele)
+        return ele2;
+    }
+
+    let card = document.getElementById("container");
+    card.innerHTML = "";
+    if (bus.length == 0) {
+        card.innerHTML = "<h2>Sorry Bus not Available</h2>"
+    }
+    bus.forEach(ele => {
+
+        //console.log(ele)
+
+        let cardDiv = createElement("div");
+        cardDiv.setAttribute("class", "card")
+
+        let heroImg = createElement("img");
+        heroImg.src = "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/1860x1050-volvo-9700-CGI1?qlt=82&wid=1024&ts=1656931444230&dpr=off&fit=constrain";
+
+        let card_content = createElement("div");
+        card_content.setAttribute("class", "card-content");
+
+        let rout = createElement("div");
+        rout.setAttribute("class", "rout_");
+        let from = createElement("H3");
+        from.innerText = ele.routeFrom;
+        let I = createElement("i");
+        I.setAttribute("class", "fa-solid fa-arrow-right-arrow-left red");
+        let to = createElement("h3");
+        to.innerText = ele.routeTo;
+        rout.append(from, I, to)
+
+        let busName = createElement("h2");
+        busName.setAttribute("class", "card-title");
+
+        let avaialableSeats = createElement("h3");
+        avaialableSeats.innerText = "Available seats :- " + ele.avaialableSeats;
+        busName.innerText = ele.busName;
+        let btn = createElement("button");
+        btn.setAttribute("class", "book-button");
+        btn.innerText = "Book Now";
+        btn.onclick = function () {
+            //console.log(ele);
+            let data = {
+                "reservationType": "active",
+                "bus": {
+                    "busId": ele.busId
+                }
+            }
+            //console.log(data)
+            postData('http://localhost:8080/reservation/add', data)
+                .then((data) => {
+                    console.log(data); // JSON data parsed by `data.json()` call
+                });
+        }
+
+        card_content.append(rout, busName, avaialableSeats, btn);
+
+        cardDiv.append(heroImg, card_content);
+        card.append(cardDiv);
+    })
 }

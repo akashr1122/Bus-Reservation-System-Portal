f6a2933e1b5088bfbc322b1d1577c4099199209d
Akhiltop <akhilyadlapalli@gmail.com>
2023-02-26 11:50:55 +0530

    added Day-6 akhil

--- BusTicket/src/main/java/com/bus/controller/LoginController.java
+++ BusTicket/src/main/java/com/bus/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.bus.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.bus.paylord.LoginDto;
 import com.bus.service.LoginService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class LoginController {
 	
 	@Autowired
